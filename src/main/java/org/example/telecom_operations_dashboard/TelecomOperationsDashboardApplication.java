e9a63a2c4eb91200a9b3c66749a560dcd1deae83
mohamedaminefradi <aminfradi614@gmail.com>
2026-01-19 15:49:09 +0100

    Initial commit: Spring Boot Telecom Operations Dashboard

---
+++ src/main/java/org/example/telecom_operations_dashboard/TelecomOperationsDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.example.telecom_operations_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomOperationsDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomOperationsDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/telecom_operations_dashboard/TelecomOperationsDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.telecom_operations_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomOperationsDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
