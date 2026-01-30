aafbfda028658c245ddeb9cf155cc2ba89040f34
mohamedaminefradi <aminfradi614@gmail.com>
2026-01-30 10:01:41 +0100

    feat: add Kafka configuration
    - Create KafkaConfig class for producer and consumer bean setup
    - Configure Kafka connection properties

---
+++ src/main/java/org/example/telecom_operations_dashboard/config/KafkaConfig.java
@@ -0,0 +1,36 @@
+package org.example.telecom_operations_dashboard.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.example.telecom_operations_dashboard.DTO.TrafficEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    @Bean
+    public ProducerFactory<String, TrafficEvent> trafficProducerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, TrafficEvent> trafficKafkaTemplate() {
+        return new KafkaTemplate<>(trafficProducerFactory());
+    }
+}
+
