790ac1857c7c9aff4939db60827f22cb655eee91
mohamedaminefradi <aminfradi614@gmail.com>
2026-03-02 22:19:56 +0100

    feat: integrate Eureka client for service discovery and update dependencies

--- src/main/java/org/example/telecom_operations_dashboard/TelecomOperationsDashboardApplication.java
+++ src/main/java/org/example/telecom_operations_dashboard/TelecomOperationsDashboardApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableCaching
 @EnableScheduling
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TelecomOperationsDashboardApplication {
     public static void main(String[] args) {
