20bae0e7271e773d65b25e1c8963d579b929e481
mohamedaminefradi <aminfradi614@gmail.com>
2026-05-13 11:48:37 +0100

    feat: implement CORS and security configuration for the gateway service [skip tests]

---
+++ gateway-service/src/main/java/org/telecom_operations_dashboard/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package org.telecom_operations_dashboard.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .anyExchange().permitAll()
+                );
+
+        return http.build();
+    }
+}
